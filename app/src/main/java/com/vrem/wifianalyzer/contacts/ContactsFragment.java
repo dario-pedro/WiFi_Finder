716e8c1aedb255589fe8a8713575e34729144511
Dário <df.pedro@campus.fct.unl.pt>
2017-01-18 22:46:27 +0000

    Finish Contacts Require testing

--- app/src/main/java/com/vrem/wifianalyzer/contacts/ContactsFragment.java
+++ app/src/main/java/com/vrem/wifianalyzer/contacts/ContactsFragment.java
@@ -25,27 +25,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.vrem.wifianalyzer.MainContext;
 import com.vrem.wifianalyzer.R;
-import com.vrem.wifianalyzer.vendor.model.VendorService;
 
 public class ContactsFragment extends ListFragment {
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.vendor_content, container, false);
-        VendorService vendorService = MainContext.INSTANCE.getVendorService();
-        //vendorAdapter = new VendorAdapter(getActivity(), vendorService.findAll());
-        //setListAdapter(vendorAdapter);
-        return view;
+        return inflater.inflate(R.layout.contacts_content, container, false);
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        VendorService vendorService = MainContext.INSTANCE.getVendorService();
-        //vendorAdapter.setVendors(vendorService.findAll());
     }
 
 }
